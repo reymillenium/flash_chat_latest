d35db9a1332e248497c6a83bae7f674e4abefdef
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-03-30 07:45:48 -0400

    Implemented modal_progress_hud on the login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 // Packages:
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 // Screens:
 import 'package:flash_chat_latest/screens/chat_screen.dart';
@@ -29,78 +30,95 @@ class _LoginScreenState extends State<LoginScreen> {
   final authHelper = Auth();
   String email;
   String password;
+  bool _saving = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'flash_logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'flash_logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
+              SizedBox(
+                height: 48.0,
+              ),
 
-            // Email Input:
-            EmailInput(
-              hintText: 'Enter your email',
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-
-            SizedBox(
-              height: 8.0,
-            ),
-
-            // Password Input:
-            PasswordInput(
-              hintText: 'Enter your password',
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-
-            SizedBox(
-              height: 24.0,
-            ),
-
-            // Login Screen button
-            Hero(
-              tag: 'login_button',
-              child: AuthButton(
-                color: Colors.lightBlueAccent,
-                onPressed: () async {
-                  // try {
-                  //   UserCredential signInResult = await _auth.signInWithEmailAndPassword(email: email, password: password);
-                  //   final User user = signInResult.user;
-                  //
-                  //   if (user != null) {
-                  //     Navigator.pushNamed(context, ChatScreen.id);
-                  //   }
-                  // } catch (e) {
-                  //   print(e);
-                  // }
-
-                  authHelper.handleSignInEmail(email, password).then((User user) {
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  }).catchError((e) => print(e));
+              // Email Input:
+              EmailInput(
+                hintText: 'Enter your email',
+                onChanged: (value) {
+                  email = value;
                 },
-                label: 'Log in',
               ),
-            )
-          ],
+
+              SizedBox(
+                height: 8.0,
+              ),
+
+              // Password Input:
+              PasswordInput(
+                hintText: 'Enter your password',
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+
+              SizedBox(
+                height: 24.0,
+              ),
+
+              // Login Screen button
+              Hero(
+                tag: 'login_button',
+                child: AuthButton(
+                  color: Colors.lightBlueAccent,
+                  onPressed: () async {
+                    setState(() {
+                      _saving = true;
+                    });
+
+                    // try {
+                    //   UserCredential signInResult = await _auth.signInWithEmailAndPassword(email: email, password: password);
+                    //   final User user = signInResult.user;
+                    //
+                    //   if (user != null) {
+                    //     Navigator.pushNamed(context, ChatScreen.id);
+                    //   }
+                    // } catch (e) {
+                    //   print(e);
+                    // }
+
+                    authHelper.handleSignInEmail(email, password).then((User user) {
+                      setState(() {
+                        _saving = false;
+                      });
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }).catchError((e) {
+                      setState(() {
+                        _saving = false;
+                      });
+                      print(e);
+                    });
+                  },
+                  label: 'Log in',
+                ),
+              )
+            ],
+          ),
         ),
+        inAsyncCall: _saving,
+        // color: Colors.red,
       ),
     );
   }

f06f0fc1c91179da96815632e2e992e3858ebbef
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-04-02 00:11:04 -0400

    Improved the rich data label of each bubble message, including the datetime and the check icon

--- lib/helpers/messages_helper.dart
+++ lib/helpers/messages_helper.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:cloud_firestore/cloud_firestore.dart'; // Allows to use FieldValue, etc
 import 'package:bubble/bubble.dart';
+import 'package:intl/intl.dart'; // Allows to use DateFormat
 
 // Screens:
 import 'package:flash_chat_latest/screens/welcome_screen.dart';
@@ -29,41 +30,101 @@ class MessagesHelper {
   }
 
   Widget createMessageWidget(QueryDocumentSnapshot messageDocument) {
-    final messageText = messageDocument.data()['text'];
     final messageSender = messageDocument.data()['sender'];
     Widget messageWidget;
 
     if (messageSender == authHelper.loggedInUser.email) {
-      messageWidget = Bubble(
-        margin: BubbleEdges.only(top: 10),
-        elevation: 1,
-        alignment: Alignment.topRight,
-        nip: BubbleNip.rightBottom,
-        color: Color.fromRGBO(225, 255, 199, 1.0),
-        child: Text(messageText, textAlign: TextAlign.right),
-      );
+      messageWidget = createOurMessageBubble(messageDocument);
     } else {
-      messageWidget = Bubble(
-        margin: BubbleEdges.only(top: 10),
-        elevation: 1,
-        alignment: Alignment.topLeft,
-        nip: BubbleNip.leftTop,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              messageSender,
-              style: TextStyle(color: Colors.red.shade200),
-            ),
-            Text(
-              messageText,
-            ),
-          ],
-        ),
-      );
+      messageWidget = createExternalMessageBubble(messageDocument);
     }
 
     return messageWidget;
+  }
+
+  Bubble createOurMessageBubble(QueryDocumentSnapshot messageDocument) {
+    final messageText = messageDocument.data()['text'];
+    RichText richTextDataLabel = createRichTextDataLabel(messageDocument);
+
+    return Bubble(
+      margin: BubbleEdges.only(top: 10),
+      elevation: 1,
+      alignment: Alignment.topRight,
+      nip: BubbleNip.rightBottom,
+      color: Color.fromRGBO(225, 255, 199, 1.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            // color: Colors.blue,
+            child: Text(messageText, textAlign: TextAlign.justify),
+          ),
+          // container,
+          richTextDataLabel,
+        ],
+      ),
+    );
+  }
+
+  Bubble createExternalMessageBubble(QueryDocumentSnapshot messageDocument) {
+    final messageText = messageDocument.data()['text'];
+    final messageSender = messageDocument.data()['sender'];
+    RichText richTextDataLabel = createRichTextDataLabel(messageDocument);
+
+    return Bubble(
+      margin: BubbleEdges.only(top: 10),
+      elevation: 1,
+      alignment: Alignment.topLeft,
+      nip: BubbleNip.leftTop,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            messageSender,
+            style: TextStyle(color: Colors.red.shade200),
+          ),
+          Text(
+            messageText,
+          ),
+          richTextDataLabel,
+        ],
+      ),
+    );
+  }
+
+  RichText createRichTextDataLabel(QueryDocumentSnapshot messageDocument) {
+    return RichText(
+      text: new TextSpan(children: [
+        TextSpan(
+          text: messageDocument.data()["created_at"] != null ? DateFormat.MMMd().add_jm().format(messageDocument.data()["created_at"].toDate()).toString() : "",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 13.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextSpan(
+          text: '   ',
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 13.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        WidgetSpan(
+            child: messageDocument.data()['isRead'] == false
+                ? Icon(
+                    Icons.done,
+                    color: Colors.greenAccent,
+                    size: 15,
+                  )
+                : Icon(
+                    Icons.done_all,
+                    color: Colors.green,
+                    size: 15,
+                  )),
+      ]),
+    );
   }
 
   Map<String, dynamic> createMessageData(String messageText) {
